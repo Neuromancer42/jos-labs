69588b4c0e10a94ccdd41a19f1db4906081a49ec
Neuromancer42 <chenyifan_1997@hotmail.com>
2018-03-04 23:11:49 +0800

    implement mon_traceback

--- kern/monitor.c
+++ kern/monitor.c
@@ -57,7 +57,12 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+  int *ebp = (int*) read_ebp();
+  while (ebp) {
+    cprintf("ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n", 
+        ebp, ebp[1], ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+    ebp = (int*)*ebp;
+  }
 	return 0;
 }
 
