0b9c89dbd9848bda2a0b99d2cc67d137c1912709
Neuromancer42 <chenyifan_1997@hotmail.com>
2018-04-23 23:35:19 +0800

    comeplete lab4 part2 COW-fork
    Signed-off-by: Neuromancer42 <chenyifan_1997@hotmail.com>

--- kern/trap.c
+++ kern/trap.c
@@ -371,7 +371,37 @@ page_fault_handler(struct Trapframe *tf)
 	//   (the 'tf' variable points at 'curenv->env_tf').
 
 	// LAB 4: Your code here.
+	if (curenv->env_pgfault_upcall) {
+		char *uxstack_top = NULL;
+		if (curenv->env_tf.tf_esp >= UXSTACKTOP - PGSIZE
+		    && curenv->env_tf.tf_esp < UXSTACKTOP)
+			uxstack_top = (char *) curenv->env_tf.tf_esp - 4;
+		else
+			uxstack_top = (char *) UXSTACKTOP;
+		uxstack_top = uxstack_top - sizeof(struct UTrapframe);
 
+		// check if 1: no page allocated; 2: no write access;
+		// 3: stack exceeded
+		user_mem_assert(curenv, uxstack_top,
+				sizeof(struct UTrapframe), PTE_W);
+
+		// set up UTrapFrame
+		struct UTrapframe *utf = (struct UTrapframe *) uxstack_top;
+		utf->utf_fault_va = fault_va;
+		utf->utf_err = tf->tf_err;
+		utf->utf_regs = tf->tf_regs;
+		utf->utf_eip = tf->tf_eip;
+		utf->utf_eflags = tf->tf_eflags;
+		utf->utf_esp = tf->tf_esp;
+
+		tf->tf_eip = (uintptr_t) curenv->env_pgfault_upcall;
+		tf->tf_esp = (uintptr_t) uxstack_top;
+
+		env_run(curenv);
+
+		// never return
+		return;
+	}
 	// Destroy the environment that caused the fault.
 	cprintf("[%08x] user fault va %08x ip %08x\n",
 		curenv->env_id, fault_va, tf->tf_eip);
